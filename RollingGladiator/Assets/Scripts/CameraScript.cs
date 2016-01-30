c1d88dac7f08b90f8e59f301b999c1d0b900aba4
TylerDinardo <tylerdinardo@L-AP236-28.CUNET.CARLETON.CA>
2016-01-30 12:22:27 -0500

    Added Camera Controller, New Volcano, Mayan Dudes, and played with physics materials

---
+++ RollingGladiator/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	Vector3 centerPos = new Vector3(0,0,0);
+	Vector3 cameraPos = new Vector3(0,0,0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Add players as required
+		centerPos.x = (player1.transform.position.x + player2.transform.position.x)/2;
+		centerPos.y = (player1.transform.position.y + player2.transform.position.y)/2;
+		centerPos.z = (player1.transform.position.z + player2.transform.position.z)/2;
+
+		cameraPos = centerPos;
+		cameraPos.y = Vector3.Distance(player1.transform.position, player2.transform.position);
+		cameraPos.x -= cameraPos.y;
+		cameraPos.z -= cameraPos.y;
+
+		if (cameraPos.y < 6)
+			cameraPos.y = 6;
+
+
+		transform.position = cameraPos;
+		transform.LookAt (centerPos);
+
+		//Camera.main.transform.position.x = p;
+		//Camera.main.transform.position.y = player2.transform.position.x - player1.transform.position.x;
+		//Camera.main.transform.position.z = player2.transform.position.z - player1.transform.position.z;
+
+
+	}
+}
