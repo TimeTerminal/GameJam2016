46c585c5a04dcb42399617f3ec3f2adc99538031
ZachSullivan <zach.r.sullivan@gmail.com>
2016-01-31 13:15:17 -0500

    Added new respawn system

--- RollingGladiator/Assets/Scripts/BoundScript.cs
+++ RollingGladiator/Assets/Scripts/BoundScript.cs
@@ -17,13 +17,20 @@ public class BoundScript : MonoBehaviour {
 		if (other.gameObject.tag == "Player" || other.gameObject.tag == "puck" ) {
 
 			Debug.Log("GRANDSLAMMMMMMMMMM");
+			StartCoroutine(Respawn(other.gameObject));
+			//other.gameObject.transform.position = new Vector3( 0, 4, 0 );
+			//other.gameObject.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
 
-			other.gameObject.transform.position = new Vector3( 0, 4, 0 );
-			other.gameObject.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
-			foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")){
-				player.transform.position = new Vector3( Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), 0 );
-				player.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
-			}
 		}
 	}
+
+	IEnumerator Respawn(GameObject player){
+
+		//foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")){
+			yield return new WaitForSeconds(2.0f);
+			player.transform.position = new Vector3( Random.Range(1.0f, 30.0f), Random.Range(1.0f, 10.0f), Random.Range(1.0f, 30.0f) );
+			player.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
+		//}
+	}
+
 }
