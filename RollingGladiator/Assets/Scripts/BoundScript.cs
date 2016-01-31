5e856b63162803292129354eebb28711c246a958
NancySDuguay <nancyduguay@L-AP236-20.CUNET.CARLETON.CA>
2016-01-31 13:12:09 -0500

    Fixed bug where both objects would reset on one's death.

--- RollingGladiator/Assets/Scripts/BoundScript.cs
+++ RollingGladiator/Assets/Scripts/BoundScript.cs
@@ -17,9 +17,9 @@ public class BoundScript : MonoBehaviour {
 		if (other.gameObject.tag == "Player" || other.gameObject.tag == "puck" ) {
 			other.gameObject.transform.position = new Vector3( 0, 4, 0 );
 			other.gameObject.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
-			foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player")){
-				player.transform.position = new Vector3( Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), 0 );
-				player.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
+			if (other.gameObject.tag == "Player") {
+				other.transform.position = new Vector3( Random.Range(1.0f, 10.0f), Random.Range(1.0f, 10.0f), 0 );
+				other.GetComponent<InputController>().myRigidBody.velocity = Vector3.zero;
 			}
 		}
 	}
