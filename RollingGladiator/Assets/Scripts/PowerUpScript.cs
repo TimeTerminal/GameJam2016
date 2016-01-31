ff3e0fa659bf44fe712b6b0eadcc987c7e3e9eb1
ZachSullivan <zach.r.sullivan@gmail.com>
2016-01-30 20:51:20 -0500

    Both players respawn now

--- RollingGladiator/Assets/Scripts/PowerUpScript.cs
+++ RollingGladiator/Assets/Scripts/PowerUpScript.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class PowerUpScript : MonoBehaviour {
 	public string powerUpType;
+	public Text modeText;
 
 	Vector3 newScale;
 	// Use this for initialization
@@ -19,26 +20,30 @@ public class PowerUpScript : MonoBehaviour {
 		
 		if (other.gameObject.tag == "Player") {
 			switch(powerUpType){
-			case "Mega": 
-				newScale = other.gameObject.transform.localScale;
-				newScale = newScale * 2;
-				other.gameObject.transform.localScale = newScale;
-				other.gameObject.GetComponent<InputController> ().myRigidBody.mass *= 2;
-				Destroy (this.gameObject);
-			break;
-			
-			case "Mini":
-				newScale = other.gameObject.transform.localScale;
-				newScale = newScale / 2;
-				other.gameObject.transform.localScale = newScale;
-				other.gameObject.GetComponent<InputController> ().myRigidBody.mass /= 2;
-				Destroy (this.gameObject);
-			break;
-
-			case "Dash":
-				other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10);
-				Destroy (this.gameObject);
+				case "Mega": 
+					modeText.text = "HEAVY MASS";
+					
+					newScale = other.gameObject.transform.localScale;
+					newScale = newScale * 2;
+					other.gameObject.transform.localScale = newScale;
+					other.gameObject.GetComponent<InputController> ().myRigidBody.mass *= 2;
+					Destroy (this.gameObject);
 				break;
+				
+				case "Mini":
+					modeText.text = "SMALL MODE";
+				
+					newScale = other.gameObject.transform.localScale;
+					newScale = newScale / 2;
+					other.gameObject.transform.localScale = newScale;
+					other.gameObject.GetComponent<InputController> ().myRigidBody.mass /= 2;
+					Destroy (this.gameObject);
+				break;
+
+				case "Dash":
+					other.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10);
+					Destroy (this.gameObject);
+					break;
 			}
 		}
 	}
